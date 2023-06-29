0a4ef2eeeb70b1cab1a43bbfb591f9e148a2d537
Guilherme Camargo <gwcamargo20@gmail.com>
2023-06-29 11:43:17 -0300

    refatorando o código

--- js/index.js
+++ js/index.js
@@ -3,36 +3,56 @@ const verGrupos = () => {
     // requisição dos dados json
     fetch('./grupos.json')
     .then(response => response.json())
-    
+
     // manipular os dados da requisição
     .then(dados => {
         console.log(dados)
 
         // mapear o array de dados
         dados.map((grupo, index) => {
-            // criar a estrutura do HTML e usar a clonagem
-            let lista_de_grupo = document.querySelector(".listaDoGrupo").cloneNode(true)
+            criarCards()
+            preencherDadosNosCards(grupo, index)
+        }) // fim dados.map
+ 
+    }) // fim then 
+     
+} // fim verGrupos 
 
-            // usar o append para colocar o conteúdo acima dentro do HTML
-            document.querySelector(".listas").append(lista_de_grupo)
+// criar cards de grupos de seleções
+const criarCards = () => {
 
-            // preencher os dados em cada card (section da lista de seleções)
-            let tituloDoGrupo = document.querySelectorAll('.tituloDoGrupo')
-            let listaDeSelecoes = document.querySelectorAll(".listaDeSelecoes")
+    // criar a estrutura do HTML e usar a clonagem
+    let listaDeGrupo = document.querySelector('.listaDoGrupo').cloneNode(true)
 
-            tituloDoGrupo[index].innerHTML = `Grupo ${grupo.grupo}`
-            listaDeSelecoes[index].innerHTML = `
-                <li><img class='bandeirap' src='images/bandeiras/${grupo.selecao1[0]}' alt='bandeira' />${grupo.selecao1[1]}</li>
-
-                <li><img class='bandeirap' src='images/bandeiras/${grupo.selecao2[0]}' alt='bandeira' />${grupo.selecao2[1]}</li>
-
-                <li><img class='bandeirap' src='images/bandeiras/${grupo.selecao3[0]}' alt='bandeira' />${grupo.selecao3[1]}</li>
-
-                <li><img class='bandeirap' src='images/bandeiras/${grupo.selecao4[0]}' alt='bandeira' />${grupo.selecao4[1]}</li>
-            `
-        })
-    })
-    
+    // usar o append
+    document.querySelector('.listas').append(listaDeGrupo)
 }
 
+// preencher os dados nos cards
+const preencherDadosNosCards = (grupo, index) => {
+
+    // destruturação do grupo e seleções
+    let {grupo: ogrupo, selecao1, selecao2, selecao3, selecao4} = grupo
+    let [bandeira1, pais1] = selecao1
+    let [bandeira2, pais2] = selecao2
+    let [bandeira3, pais3] = selecao3
+    let [bandeira4, pais4] = selecao4
+
+    let tituloDoGrupo = document.querySelectorAll(".tituloDoGrupo")
+    let listaDeSelecoes = document.querySelectorAll(".listaDeSelecoes")
+
+    tituloDoGrupo[index].innerHTML = `Grupo ${ogrupo}`
+    listaDeSelecoes[index].innerHTML = `
+
+        <li><img class='bandeirap' src='images/bandeiras/${bandeira1}' alt='bandeira'/>${pais1}</li> 
+        
+        <li><img class='bandeirap' src='images/bandeiras/${bandeira2}' alt='bandeira'/>${pais2}</li> 
+
+        <li><img class='bandeirap' src='images/bandeiras/${bandeira3}' alt='bandeira'/>${pais3}</li> 
+
+        <li><img class='bandeirap' src='images/bandeiras/${bandeira4}' alt='bandeira'/>${pais4}</li> 
+    `
+}  // fim preencher os dados nos cards
+
+// executar verGrupos para criar os cards / section dos grupos e preencher os dados
 verGrupos()
