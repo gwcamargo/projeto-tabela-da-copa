81f2e08b51f48a309f2d3a47f2642862340eaf00
Guilherme Camargo <gwcamargo20@gmail.com>
2023-06-29 10:56:36 -0300

    corrigindo alguns erros

--- js/index.js
+++ js/index.js
@@ -1,23 +1,38 @@
-let grupos = []
-let selecoes = []
+const verGrupos = () => {
 
-let letra_do_grupo = document.querySelector('.letra_do_grupo').value
-let titulo_grupo = document.querySelector('.titulo_grupo')
-let lista_grupo = document.querySelector('.lista_grupo')
-
-const verGrupos = (letra_do_grupo) => { 
+    // requisição dos dados json
     fetch('./grupos.json')
     .then(response => response.json())
+    
+    // manipular os dados da requisição
     .then(dados => {
         console.log(dados)
-        titulo_grupo.innerHTML = `Grupo ${dados[letra_do_grupo].grupo}`
-        lista_grupo.innerHTML = `
-            <li>${dados[letra_do_grupo].selecao1}</li>
-            <li>${dados[letra_do_grupo].selecao2}</li>
-            <li>${dados[letra_do_grupo].selecao3}</li>
-            <li>${dados[letra_do_grupo].selecao4}</li>
-        `
+
+        // mapear o array de dados
+        dados.map((grupo, index) => {
+            // criar a estrutura do HTML e usar a clonagem
+            let lista_de_grupo = document.querySelector(".listaDoGrupo").cloneNode(true)
+
+            // usar o append para colocar o conteúdo acima dentro do HTML
+            document.querySelector(".listas").append(lista_de_grupo)
+
+            // preencher os dados em cada card (section da lista de seleções)
+            let tituloDoGrupo = document.querySelectorAll('.tituloDoGrupo')
+            let listaDeSelecoes = document.querySelectorAll(".listaDeSelecoes")
+
+            tituloDoGrupo[index].innerHTML = `Grupo ${grupo.grupo}`
+            listaDeSelecoes[index].innerHTML = `
+                <li><img class='bandeirap' src='images/bandeiras/${grupo.selecao1[0]}' alt='bandeira' />${grupo.selecao1[1]}</li>
+
+                <li><img class='bandeirap' src='images/bandeiras/${grupo.selecao2[0]}' alt='bandeira' />${grupo.selecao2[1]}</li>
+
+                <li><img class='bandeirap' src='images/bandeiras/${grupo.selecao3[0]}' alt='bandeira' />${grupo.selecao3[1]}</li>
+
+                <li><img class='bandeirap' src='images/bandeiras/${grupo.selecao4[0]}' alt='bandeira' />${grupo.selecao4[1]}</li>
+            `
+        })
     })
+    
 }
 
-verGrupos(letra_do_grupo)
+verGrupos()
